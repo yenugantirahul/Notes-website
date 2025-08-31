7fb351b94393f892e4c93790f9d35f1a6c5d8ff5
Yenuganti Rahul <yenugantirahul@gmail.com>
2025-08-31 20:46:44 +0530

    Fixed bug in my notes app

--- src/main.tsx
+++ src/main.tsx
@@ -5,17 +5,14 @@ import App from "./App.tsx";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 import { NotesProvider } from "./context/NotesProvider.tsx";
 import { BrowserRouter } from "react-router-dom";
-import { store } from "./context/store.tsx";
 import { Provider } from "react-redux";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark">
       <NotesProvider>
-        <Provider store={store}>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </Provider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </NotesProvider>
     </ThemeProvider>
   </StrictMode>
