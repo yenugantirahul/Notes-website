07cd8808a707ea5c1fe033a6869b41e135817682
Yenuganti Rahul <yenugantirahul@gmail.com>
2025-08-31 21:02:55 +0530

    Fixed a small bug

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,15 @@ import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 import { NotesProvider } from "./context/NotesProvider.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark">
       <NotesProvider>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </NotesProvider>
     </ThemeProvider>
   </StrictMode>
